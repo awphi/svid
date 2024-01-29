65ff3be800717fd87374cdef286c736ae5a56284
Adam Hodson <jellymoos3@gmail.com>
2024-01-29 18:52:33 +0000

    use tailwind vscode plugin in .css files

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,6 +1,6 @@
 import App from "./App.svelte";
-import "./index.pcss";
-import "./splitpanes.pcss";
+import "./index.css";
+import "./splitpanes.css";
 
 const app = new App({
   target: document.getElementById("app")!,
